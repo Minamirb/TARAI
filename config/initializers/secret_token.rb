0591c5e8f73c3dbab02daab3c4adf4351257a8ba
Jun Fukaya <fukajun.shark@gmail.com>
2011-06-19 10:10:03 +0900

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tarai::Application.config.secret_token = '7d29a3cce1180f5fe885d7646ed1e51a29adf429ea52e6fa32ce0a84d66858f0216bce48f46a34cf2e2a571d3f74841b86c6958996a75d03a48c4c9448fd0b8e'
