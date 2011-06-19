9d3624174b52652df3543056c21a490f40fdb158
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2011-06-20 01:03:20 +0900

    tempnate から拾ってきたテンプレートを適用。users#welcome を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tarai::Application.routes.draw do
+
   devise_for :users
 
   resources :messages
@@ -53,6 +54,7 @@ Tarai::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'users#welcome'
 
   # See how all your routes lay out with "rake routes"
 
