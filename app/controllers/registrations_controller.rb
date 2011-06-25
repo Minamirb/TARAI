8e8a993f879dd141792c3d7a8be3c8b6107a3dc4
Jun Fukaya <fukajun.shark@gmail.com>
2011-06-25 15:54:12 +0900

    ユーザー情報が認証なしにアクセスできてしまっていたバグの修正

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
+  # 認証の対象にshowを含める
+  prepend_before_filter :authenticate_scope!, :only => [:show, :edit, :update, :destroy]
+
   def show
     @user = current_user
   end
+
 end
